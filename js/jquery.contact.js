8c5f797f3322dde6fd4f193ba1dfcc40fb8e1404
瞿龙俊 <qulongjun@qulongjundeMacBook-Pro.local>
2016-05-20 16:00:51 +0800

    完善教育经历

--- js/jquery.contact.js
+++ js/jquery.contact.js
@@ -1,38 +1,40 @@
-(function($) { 
-	"use strict";
-	
-jQuery(document).ready(function(){
-	$('#cform').submit(function(){
+(function ($) {
+    "use strict";
 
-		var action = $(this).attr('action');
+    jQuery(document).ready(function () {
+        $('#cform').submit(function () {
 
-		$("#message").slideUp(750,function() {
-		$('#message').hide();
+            var action = $(this).attr('action');
 
- 		$('#submit')
-			.before('<img src="images/ajax-loader.gif" class="contact-loader" />')
-			.attr('disabled','disabled');
+            $("#message").slideUp(750, function () {
+                $('#message').hide();
 
-		$.post(action, {
-			name: $('#name').val(),
-			email: $('#email').val(),
-			comments: $('#comments').val(),
-		},
-			function(data){
-				document.getElementById('message').innerHTML = data;
-				$('#message').slideDown('slow');
-				$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
-				$('#submit').removeAttr('disabled');
-				if(data.match('success') != null) $('#cform').slideUp('slow');
-			}
-		);
+                $('#submit')
+                    .before('<img src="images/ajax-loader.gif" class="contact-loader" />')
+                    .attr('disabled', 'disabled');
 
-		});
+                $.post(action, {
+                        name: $('#name').val(),
+                        email: $('#email').val(),
+                        comments: $('#comments').val(),
+                    },
+                    function (data) {
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#cform img.contact-loader').fadeOut('slow', function () {
+                            $(this).remove()
+                        });
+                        $('#submit').removeAttr('disabled');
+                        if (data.match('success') != null) $('#cform').slideUp('slow');
+                    }
+                );
 
-		return false;
+            });
 
-	});
+            return false;
 
-});
+        });
+
+    });
 
 }(jQuery));

44aa64cb26e877db7f07ede1438bbb797a14c06b
瞿龙俊 <qulongjun@qulongjundeMacBook-Pro.local>
2016-06-14 18:06:24 +0800

    增加接收留言端口

--- js/jquery.contact.js
+++ js/jquery.contact.js
@@ -3,20 +3,17 @@
 
     jQuery(document).ready(function () {
         $('#cform').submit(function () {
-
             var action = $(this).attr('action');
-
             $("#message").slideUp(750, function () {
                 $('#message').hide();
-
                 $('#submit')
                     .before('<img src="images/ajax-loader.gif" class="contact-loader" />')
                     .attr('disabled', 'disabled');
 
                 $.post(action, {
-                        name: $('#name').val(),
+                        nickname: $('#name').val(),
                         email: $('#email').val(),
-                        comments: $('#comments').val(),
+                        content: $('#comments').val(),
                     },
                     function (data) {
                         document.getElementById('message').innerHTML = data;
